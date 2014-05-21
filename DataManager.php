b6281e674bb621ae4770b6501d36123a36c080a5
Eric Gubiani <eric.gubiani93@gmail.com>
2014-05-20 23:05:36 -0400

    Update DataManager.php
    added to the add products function and added some helper functions for it

--- DataManager.php
+++ DataManager.php
@@ -50,8 +50,11 @@ class DataManager{
 	//adds a new product into the database, requires interaction with an html form
 	public function addProduct($prodData){
 		$query = $this->connection->prepare(
-        "INSERT INTO Products (catID, productName) VALUES (:catID,:prodName)");
-		$prodData=[':catID'=> $prodData['catID'], ':prodName'=> $prodData['prodName']];
+        "INSERT INTO Products (catID, productName, prodAbv) VALUES (:catID,:prodName,:prodAbv)");
+		
+		//add check for if product already exists or has existed in the past
+		
+		$prodData=[':catID'=> $prodData['catID'], ':prodName'=> $prodData['prodName'], ':prodAbv'=>$prodData['prodAbv']];
 		
 		$query->execute($prodData);
 	}
@@ -59,7 +62,38 @@ class DataManager{
 	//this method must take in product details and a number of items to add, requires html form
 	//it must also generate a unique product code for each item and insert it with the item
 	public function addItems($itemData){
+		$numItems=$itemData['numItems'];
+		
+		$prodCode= genProdCode($itemData);//generate prodCode here
+		
+		for($i=0;$i<$numItems;$i++){
+			$query = $this->connection->prepare(
+			 "INSERT INTO Items (nameID, catID, prodCode) VALUES (:nameID, :catID,:prodCode)");
+			$itemData=[':productID'=> $itemData['productID'], ':catID'=>$itemData['catID'], ':prodCode'=> $prodCode];
+		
+			$query->execute($prodData);	
+		}
+	}
 	
+	//generates a unique product code
+	public function genProdCode($itemData){
+		$productID=$itemData['productID'];
+		$abv=getProdAbv($productID);
+		
+		//find largest value of prodCode for that item in stock and add 1 to it
+		//sum the product abreviated code and the number code to generate the prodCode
+		
+	}
+	
+	//gets the 3 letter abbreviated code for the product
+	public function getProdAbv($productID){
+		$query=$this->connection->prepare("SELECT * FROM Products WHERE productID=:productID"); 
+		
+		$data=[':productID'=>$productID];
+		
+		$query->execute($data);
+		
+		return $query['productAbv'];
 	}
 	
 	//removes an item from inventory and places it in the archive database with the timestamp of removal for data generation
